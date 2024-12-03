21a3691eccc93580c215ae58892a368b6c4c217a
ADARSH SINGH <9792051262a@gmail.com>
2024-12-04 00:08:41 +0530

    removed header from axiosConfig file

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -4,9 +4,6 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: 'https://new-education-era-backend.onrender.com', // Backend URL
   withCredentials: true, // Allow sending cookies with requests
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
 
 export default axiosInstance;
