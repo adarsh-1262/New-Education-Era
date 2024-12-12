d4e1b2d8f7ec2c630895f416b26ddfaeeb3619ce
Narendra Maurya <amanlearntoenvent17@gmail.com>
2024-12-12 16:48:39 +0530

    navbar minor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -83,7 +83,7 @@ export default function Navbar() {
             {isLoggedIn ? (
               <div className="flex items-center gap-6">
                 <img title="Profile"
-                onClick={() => navigate(/${userRole}/dashboard, { replace: true })}
+                onClick={() => navigate(`/${userRole}/dashboard`, { replace: true })}
                 src={profilePicture}
                 alt="Profile"
                 className="w-12 h-12 border-2 border-white rounded-full cursor-pointer hover:opacity-80 transition-opacity"
