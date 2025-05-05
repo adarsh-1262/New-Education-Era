64fa81364b0a266ff6ad483b7588f7f0b16334db
Narendra Maurya <amanlearntoenvent17@gmail.com>
2025-05-05 23:12:30 +0530

    changes the vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        '@fullcalendar/core',
-        '@fullcalendar/react',
-        '@fullcalendar/daygrid'
-      ]
-    }
-  }
 })
