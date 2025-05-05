cbd78dae3440479bd57cfb30bd7ffad56bd35c96
ADARSH SINGH <9792051262a@gmail.com>
2025-05-05 22:24:39 +0530

    installed some more packages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: false,
-  },
+    rollupOptions: {
+      external: [
+        '@fullcalendar/core',
+        '@fullcalendar/react',
+        '@fullcalendar/daygrid'
+      ]
+    }
+  }
 })
